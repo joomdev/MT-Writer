6913970a813e1649e90b8a1b0f1f7eae24187cb1
actuallyakash <akash_gupta@hotmail.com>
2019-10-31 12:13:35 +0530

    Added default values filter

--- inc/defaults.php
+++ inc/defaults.php
@@ -139,144 +139,144 @@ if ( ! function_exists( 'mtwriter_get_default_fonts' ) ) {
 	 * Set default options.
 	 */
 	function mtwriter_get_default_fonts( ) {
-		$defaults = array(
-			'body_fontfamily' => 'Roboto',
-			'body_fontsize' => '16',
-			'body_fontsize_unit' => 'px',
-			'body_texttransform' => 'none',
-			'body_alt_fontfamily' => 'Arial',
-            'body_letterspacing' => '0',
-            'body_fontweight' => 'normal',
-            'body_lineheight' => '1.2', // no unit
-            
-            'h1_fontfamily' => 'Heebo',
-			'h1_fontsize' => '40',
-			'h1_fontsize_unit' => 'px',
-			'h1_texttransform' => 'none',
-			'h1_alt_fontfamily' => 'Arial',
-            'h1_letterspacing' => '0',
-            'h1_fontweight' => '700',
-            'h1_lineheight' => '1.2', // no unit
+        return apply_filters('mtwriter_default_fonts',
+            array(
+                'body_fontfamily' => 'Roboto',
+                'body_fontsize' => '16',
+                'body_fontsize_unit' => 'px',
+                'body_texttransform' => 'none',
+                'body_alt_fontfamily' => 'Arial',
+                'body_letterspacing' => '0',
+                'body_fontweight' => 'normal',
+                'body_lineheight' => '1.2', // no unit
+                
+                'h1_fontfamily' => 'Heebo',
+                'h1_fontsize' => '40',
+                'h1_fontsize_unit' => 'px',
+                'h1_texttransform' => 'none',
+                'h1_alt_fontfamily' => 'Arial',
+                'h1_letterspacing' => '0',
+                'h1_fontweight' => '700',
+                'h1_lineheight' => '1.2', // no unit
 
-            'h2_fontfamily' => 'Heebo',
-			'h2_fontsize' => '32',
-			'h2_fontsize_unit' => 'px',
-			'h2_texttransform' => 'none',
-			'h2_alt_fontfamily' => 'Arial',
-            'h2_letterspacing' => '1',
-            'h2_fontweight' => '700',
-            'h2_lineheight' => '1.2', // no unit
+                'h2_fontfamily' => 'Heebo',
+                'h2_fontsize' => '32',
+                'h2_fontsize_unit' => 'px',
+                'h2_texttransform' => 'none',
+                'h2_alt_fontfamily' => 'Arial',
+                'h2_letterspacing' => '1',
+                'h2_fontweight' => '700',
+                'h2_lineheight' => '1.2', // no unit
 
-            'h3_fontfamily' => 'Heebo',
-			'h3_fontsize' => '32',
-			'h3_fontsize_unit' => 'px',
-			'h3_texttransform' => 'none',
-			'h3_alt_fontfamily' => 'Arial',
-            'h3_letterspacing' => '1',
-            'h3_fontweight' => '700',
-            'h3_lineheight' => '1.2', // no unit
+                'h3_fontfamily' => 'Heebo',
+                'h3_fontsize' => '32',
+                'h3_fontsize_unit' => 'px',
+                'h3_texttransform' => 'none',
+                'h3_alt_fontfamily' => 'Arial',
+                'h3_letterspacing' => '1',
+                'h3_fontweight' => '700',
+                'h3_lineheight' => '1.2', // no unit
 
-            'h4_fontfamily' => 'Heebo',
-			'h4_fontsize' => '24',
-			'h4_fontsize_unit' => 'px',
-			'h4_texttransform' => 'none',
-			'h4_alt_fontfamily' => 'Arial',
-            'h4_letterspacing' => '1',
-            'h4_fontweight' => '700',
-            'h4_lineheight' => '1.2', // no unit
+                'h4_fontfamily' => 'Heebo',
+                'h4_fontsize' => '24',
+                'h4_fontsize_unit' => 'px',
+                'h4_texttransform' => 'none',
+                'h4_alt_fontfamily' => 'Arial',
+                'h4_letterspacing' => '1',
+                'h4_fontweight' => '700',
+                'h4_lineheight' => '1.2', // no unit
 
-            'h5_fontfamily' => 'Heebo',
-			'h5_fontsize' => '20',
-			'h5_fontsize_unit' => 'px',
-			'h5_texttransform' => 'none',
-			'h5_alt_fontfamily' => 'Arial',
-            'h5_letterspacing' => '1',
-            'h5_fontweight' => '700',
-            'h5_lineheight' => '1.2', // no unit
+                'h5_fontfamily' => 'Heebo',
+                'h5_fontsize' => '20',
+                'h5_fontsize_unit' => 'px',
+                'h5_texttransform' => 'none',
+                'h5_alt_fontfamily' => 'Arial',
+                'h5_letterspacing' => '1',
+                'h5_fontweight' => '700',
+                'h5_lineheight' => '1.2', // no unit
 
-            'h6_fontfamily' => 'Heebo',
-			'h6_fontsize' => '16',
-			'h6_fontsize_unit' => 'px',
-			'h6_texttransform' => 'none',
-			'h6_alt_fontfamily' => 'Arial',
-            'h6_letterspacing' => '1',
-            'h6_fontweight' => '700',
-            'h6_lineheight' => '1.2', // no unit
+                'h6_fontfamily' => 'Heebo',
+                'h6_fontsize' => '16',
+                'h6_fontsize_unit' => 'px',
+                'h6_texttransform' => 'none',
+                'h6_alt_fontfamily' => 'Arial',
+                'h6_letterspacing' => '1',
+                'h6_fontweight' => '700',
+                'h6_lineheight' => '1.2', // no unit
 
-            'logo_fontfamily' => 'Roboto',
-			'logo_fontsize' => '20',
-			'logo_fontsize_unit' => 'px',
-			'logo_texttransform' => 'none',
-			'logo_alt_fontfamily' => 'Arial',
-            'logo_letterspacing' => '0',
-            'logo_fontweight' => 'normal',
-            'logo_lineheight' => '0', // no unit
+                'logo_fontfamily' => 'Roboto',
+                'logo_fontsize' => '20',
+                'logo_fontsize_unit' => 'px',
+                'logo_texttransform' => 'none',
+                'logo_alt_fontfamily' => 'Arial',
+                'logo_letterspacing' => '0',
+                'logo_fontweight' => 'normal',
+                'logo_lineheight' => '0', // no unit
 
-            'mainmenu_fontfamily' => 'Roboto',
-			'mainmenu_fontsize' => '16',
-			'mainmenu_fontsize_unit' => 'px',
-			'mainmenu_texttransform' => 'none',
-			'mainmenu_alt_fontfamily' => 'Arial',
-            'mainmenu_letterspacing' => '0',
-            'mainmenu_fontweight' => 'normal',
-            'mainmenu_lineheight' => '1.2', // no unit
+                'mainmenu_fontfamily' => 'Roboto',
+                'mainmenu_fontsize' => '16',
+                'mainmenu_fontsize_unit' => 'px',
+                'mainmenu_texttransform' => 'none',
+                'mainmenu_alt_fontfamily' => 'Arial',
+                'mainmenu_letterspacing' => '0',
+                'mainmenu_fontweight' => 'normal',
+                'mainmenu_lineheight' => '1.2', // no unit
 
-            'dropdown_fontfamily' => 'Roboto',
-			'dropdown_fontsize' => '16',
-			'dropdown_fontsize_unit' => 'px',
-			'dropdown_texttransform' => 'none',
-			'dropdown_alt_fontfamily' => 'Arial',
-            'dropdown_letterspacing' => '0',
-            'dropdown_fontweight' => 'normal',
-            'dropdown_lineheight' => '1.2', // no unit
+                'dropdown_fontfamily' => 'Roboto',
+                'dropdown_fontsize' => '16',
+                'dropdown_fontsize_unit' => 'px',
+                'dropdown_texttransform' => 'none',
+                'dropdown_alt_fontfamily' => 'Arial',
+                'dropdown_letterspacing' => '0',
+                'dropdown_fontweight' => 'normal',
+                'dropdown_lineheight' => '1.2', // no unit
 
-            'entrytitle_fontfamily' => 'Roboto',
-			'entrytitle_fontsize' => '20',
-			'entrytitle_fontsize_unit' => 'px',
-			'entrytitle_texttransform' => 'none',
-			'entrytitle_alt_fontfamily' => 'Arial',
-            'entrytitle_letterspacing' => '1',
-            'entrytitle_fontweight' => '600',
-            'entrytitle_lineheight' => '1.2', // no unit
+                'entrytitle_fontfamily' => 'Roboto',
+                'entrytitle_fontsize' => '20',
+                'entrytitle_fontsize_unit' => 'px',
+                'entrytitle_texttransform' => 'none',
+                'entrytitle_alt_fontfamily' => 'Arial',
+                'entrytitle_letterspacing' => '1',
+                'entrytitle_fontweight' => '600',
+                'entrytitle_lineheight' => '1.2', // no unit
 
-            'posttitle_fontfamily' => 'Roboto',
-			'posttitle_fontsize' => '32',
-			'posttitle_fontsize_unit' => 'px',
-			'posttitle_texttransform' => 'none',
-			'posttitle_alt_fontfamily' => 'Arial',
-            'posttitle_letterspacing' => '1',
-            'posttitle_fontweight' => '700',
-            'posttitle_lineheight' => '1.2', // no unit
+                'posttitle_fontfamily' => 'Roboto',
+                'posttitle_fontsize' => '32',
+                'posttitle_fontsize_unit' => 'px',
+                'posttitle_texttransform' => 'none',
+                'posttitle_alt_fontfamily' => 'Arial',
+                'posttitle_letterspacing' => '1',
+                'posttitle_fontweight' => '700',
+                'posttitle_lineheight' => '1.2', // no unit
 
-            'meta_fontfamily' => 'Roboto',
-			'meta_fontsize' => '12',
-			'meta_fontsize_unit' => 'px',
-			'meta_texttransform' => 'none',
-			'meta_alt_fontfamily' => 'Arial',
-            'meta_letterspacing' => '1',
-            'meta_fontweight' => '600',
-            'meta_lineheight' => '1.2', // no unit
+                'meta_fontfamily' => 'Roboto',
+                'meta_fontsize' => '12',
+                'meta_fontsize_unit' => 'px',
+                'meta_texttransform' => 'none',
+                'meta_alt_fontfamily' => 'Arial',
+                'meta_letterspacing' => '1',
+                'meta_fontweight' => '600',
+                'meta_lineheight' => '1.2', // no unit
 
-            'widgettitle_fontfamily' => 'Roboto',
-			'widgettitle_fontsize' => '16',
-			'widgettitle_fontsize_unit' => 'px',
-			'widgettitle_texttransform' => 'none',
-			'widgettitle_alt_fontfamily' => 'Arial',
-            'widgettitle_letterspacing' => '0',
-            'widgettitle_fontweight' => '500',
-            'widgettitle_lineheight' => '1.2', // no unit
-            
-            'copyright_fontfamily' => 'Roboto',
-			'copyright_fontsize' => '16',
-			'copyright_fontsize_unit' => 'px',
-			'copyright_texttransform' => 'none',
-			'copyright_alt_fontfamily' => 'Arial',
-            'copyright_letterspacing' => '0',
-            'copyright_fontweight' => 'normal',
-            'copyright_lineheight' => '1.2', // no unit
-		);
-
-		return $defaults;
+                'widgettitle_fontfamily' => 'Roboto',
+                'widgettitle_fontsize' => '16',
+                'widgettitle_fontsize_unit' => 'px',
+                'widgettitle_texttransform' => 'none',
+                'widgettitle_alt_fontfamily' => 'Arial',
+                'widgettitle_letterspacing' => '0',
+                'widgettitle_fontweight' => '500',
+                'widgettitle_lineheight' => '1.2', // no unit
+                
+                'copyright_fontfamily' => 'Roboto',
+                'copyright_fontsize' => '16',
+                'copyright_fontsize_unit' => 'px',
+                'copyright_texttransform' => 'none',
+                'copyright_alt_fontfamily' => 'Arial',
+                'copyright_letterspacing' => '0',
+                'copyright_fontweight' => 'normal',
+                'copyright_lineheight' => '1.2', // no unit
+            )
+        );
 	}
 }
 
